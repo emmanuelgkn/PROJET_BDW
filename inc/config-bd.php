25bca6fff610f63f5283cecc880d90a72d345216
emmanuelgkn <39710729+emmanuelgkn@users.noreply.github.com>
2023-06-29 06:59:10 +0200

    Add files via upload

---
+++ inc/config-bd.php
@@ -0,0 +1,6 @@
+<?php
+	define('SERVEUR', 'localhost');
+	define('UTILISATEUR', 'p2100037'); // votre login, par exemple p1234567
+	define('MOTDEPASSE', 'Rehire22Slogan'); // votre mot de passe, par exemple Tea4Two
+	define('BDD', 'p2100037'); // votre BD, par exemple p1234567
+?>
